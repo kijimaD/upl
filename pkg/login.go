77543b39823eb37409ea6c0ac1463d1becb495da
Kijima Daigo <norimaking777@gmail.com>
2023-10-19 00:21:15 +0900

    chore: trash headers

--- pkg/login.go
+++ pkg/login.go
@@ -16,22 +16,7 @@ func buildLogin() string {
 	basecmd := `%s %s \
   -s \
   -c - \
-  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-  -H 'Accept-Language: en-US,en;q=0.9,ja-JP;q=0.8,ja;q=0.7' \
-  -H 'Cache-Control: max-age=0' \
-  -H 'Connection: keep-alive' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -H 'Origin: http://localhost:7777' \
-  -H 'Referer: http://localhost:7777/' \
-  -H 'Sec-Fetch-Dest: document' \
-  -H 'Sec-Fetch-Mode: navigate' \
-  -H 'Sec-Fetch-Site: same-origin' \
-  -H 'Sec-Fetch-User: ?1' \
-  -H 'Upgrade-Insecure-Requests: 1' \
-  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' \
-  -H 'sec-ch-ua: "Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "Linux"' \
   --data-raw 'fm_usr=%s&fm_pwd=%s'`
 	cmd := fmt.Sprintf(basecmd,
 		COMMAND,
--- pkg/upl.go
+++ pkg/upl.go
@@ -17,22 +17,7 @@ func buildUpload(cookie string) string {
 	basecmd := `%s %s \
   %s \
   -# \
-  -H 'Accept: application/json' \
-  -H 'Accept-Language: ja,en-US;q=0.9,en;q=0.8' \
-  -H 'Cache-Control: no-cache' \
-  -H 'Connection: keep-alive' \
   -H 'Cookie: filemanager=%s' \
-  -H 'Origin: http://localhost' \
-  -H 'Pragma: no-cache' \
-  -H 'Referer: http://localhost/uploader?p=&upload' \
-  -H 'Sec-Fetch-Dest: empty' \
-  -H 'Sec-Fetch-Mode: cors' \
-  -H 'Sec-Fetch-Site: same-origin' \
-  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' \
-  -H 'X-Requested-With: XMLHttpRequest' \
-  -H 'sec-ch-ua: "Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"' \
-  -H 'sec-ch-ua-mobile: ?0' \
-  -H 'sec-ch-ua-platform: "Linux"' \
   --compressed \
   -F "p=" \
   -F "fullpath=%s" \
