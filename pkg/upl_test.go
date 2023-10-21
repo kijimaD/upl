53d845ba83cd562a7ad1d784b0f33f667e079cab
Kijima Daigo <norimaking777@gmail.com>
2023-10-21 21:50:26 +0900

    test: use assertion

--- pkg/upl_test.go
+++ pkg/upl_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUpload(t *testing.T) {
@@ -25,7 +27,8 @@ func TestUpload(t *testing.T) {
 	}
 }
 
-func TestUploadFail(t *testing.T) {
+// クッキーが無効だとエラーを返す
+func TestUploadFailInvalidCookie(t *testing.T) {
 	file, err := os.Create(UPLOAD_TARGET)
 	if err != nil {
 		t.Error(err)
@@ -35,8 +38,19 @@ func TestUploadFail(t *testing.T) {
 	b := &bytes.Buffer{}
 	task := NewTask(b)
 	err = task.upload("invalid cookie")
-	// クッキーが無効だと失敗する
-	if err != FailUploadError {
+
+	assert.Equal(t, FailUploadError, err)
+}
+
+// upload.zipファイルがないとエラーを返す
+func TestUploadFailFileNotFound(t *testing.T) {
+	b := &bytes.Buffer{}
+	task := NewTask(b)
+	cookie, err := task.login()
+	if err != nil {
 		t.Error(err)
 	}
+	err = task.upload(cookie)
+
+	assert.Equal(t, TargetFileNotFoundError, err)
 }
